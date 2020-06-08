0ef1b1f079a070d10ad26a3fd9aa52d7d872491e
Cédric Audy <cedric.audy08@gmail.com>
2020-06-07 20:43:12 -0400

    playing aroung with weights

--- rgb_to_human_color.py
+++ rgb_to_human_color.py
@@ -29,6 +29,11 @@ y = np.array(rows)
 
 model = Sequential()
 model.add(Dense(3, input_dim=3))
+
+print(model.layers[0].get_weights())
+model.layers[0].set_weights([np.array([[0.2,0.2,0.2],[0.6,0.6,0.6],[0.1,0.1,0.1]]), np.array([0,1,0])])
+
+
 model.add(Activation('softsign'))
 model.add(Dense(9))
 model.add(Activation('softmax'))
